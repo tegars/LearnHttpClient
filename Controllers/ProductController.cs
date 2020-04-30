a102923a0a939d2944eb73a51f8f3caff083b268
tegarsweefer <tegar.s@weefer.co.id>
2020-04-30 14:44:40 +0700

    persiapan data yang akan dipanggil

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearnHttpClient.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnHttpClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/Product
+        [HttpGet]
+        public ActionResult<List<Product>> Get()
+        {
+            List<Product> products = new List<Product>();
+            var product1 = new Product() { 
+                Id = Guid.NewGuid(),
+                Name = "Kickers",
+                Harga = "Rp. 800.000",
+                Warna = "Merah"
+            };
+            var product2 = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Gucci",
+                Harga = "Rp. 500.000",
+                Warna = "Hijau"
+            };
+            products.Add(product1);
+            products.Add(product2);
+            return products;
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kickers, id: "+id,
+                Harga = "Rp. 800.000",
+                Warna = "Merah"
+            };
+            return Ok(product);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnHttpClient.Model
+{
+    public class Product
+    {
+        public Guid Id { set; get; }
+        public string Name { set; get; }
+        public string Harga { set; get; }
+        public string Warna { set; get; }
+    }
+}
